c2ecb6e4baec8b005917e2ebb9a0f1b15831eda9
Buddy Collins <50601293+Buddytheelff@users.noreply.github.com>
2021-11-18 10:01:19 -0700

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -3,11 +3,24 @@
 
 
 var year = ["1900","1910","1920","1930","1940","1950","1960","1970","1980","1990","2000","2010"];
-var data;
+var data =  [
+  {x:"1900", y:0.1 * 100},
+  {x:"1910", y:0.2 * 100},
+  {x:"1920", y:0.3 * 100},
+  {x:"1930", y:0.35 * 100},
+  {x:"1940", y:0.4 * 100},
+  {x:"1950", y:0.45 * 100},
+  {x:"1960", y:0.5 * 100},
+  {x:"1970", y:0.55 * 100},
+  {x:"1980", y:0.6 * 100},
+  {x:"1990", y:0.65 * 100},
+  {x:"2000", y:0.7 * 100},
+  {x:"2010", y:0.75 * 100}];
 // append the svg object to the body of the page
 
 function createChart(){
   var svg = d3.select("#chartid").append("svg:svg")
+  .attr("id","chartsvg")
   .attr("width", 800)//canvasWidth)
   .attr("height", 300),//canvasHeight);
   margin = { top: 20, right: 20, bottom: 30, left: 70 },
@@ -45,15 +58,15 @@ function createChart(){
   .attr("stroke", "steelblue")
   .attr("stroke-width", 1.5)
   .attr("d", d3.line()
-  .x(function(d) {return x(data.x) })
-  .y(function(d) {return y(data.y) })
+  .x(d => data.x)
+  .y(d => data.y)
 )
-
+console.log(data);
 
 };
 
 function createLine(properties){
-
+  d3.select("#chartsvg").remove();
   data = [
     {x:"1900", y:properties.F1900_mean * 100},
     {x:"1910", y:properties.F1910_mean * 100},
@@ -67,7 +80,6 @@ function createLine(properties){
     {x:"1990", y:properties.F1990_mean * 100},
     {x:"2000", y:properties.F2000_mean * 100},
     {x:"2010", y:properties.F2010_mean * 100}];
-    console.log(data);
-
+    createChart();
 
 }
