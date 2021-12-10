158cbfb7e622b75637dc12d6f41aea22e737af05
Buddy Collins <50601293+Buddytheelff@users.noreply.github.com>
2021-12-10 14:53:41 -0700

    working legend

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 //Author Buddy Collins
 "use strict"
+//------------------------------------------------------------------------------------------------------------------
 var geojson;
 var map;
 var index = 0;
@@ -7,15 +8,13 @@ var info = L.control();
 var polygonStyle = {
   "weight": 3,
   "color": "#6699ff"};
-var year = ["1900","1910","1920","1930","1940","1950","1960","1970","1980","1990","2000","2010"];
+var year = ["1900","1910","1920","1930","1940","1950","1960","1970","1980","1990","2000","2010","2020"];
 var slideArr = [];
-var chlorArray = [];
+//------------------------------------------------------------------------------------------------------------------
 window.onload = function () {
     createMap();
     createChart();
 };
-
-
 //-------------------------------------------------------------------------------------------------------------------
 function createMap(){
 // create a leaflet map with a dark basemap
@@ -28,7 +27,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/buddytheelf/ckvsqoj1h11xk14oafqc8f
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 }).addTo(map);
 
-
+//------------------------------------------------------------------------------------------------------------------
 // adds the geojson data
 $.getJSON("data/MSA.geojson", function(response){
     geojson = L.geoJSON(response, {
@@ -40,78 +39,7 @@ $.getJSON("data/MSA.geojson", function(response){
     info.addTo(map);
     createSlider(response);
 });
-};
-
-//When adding the info
-info.onAdd = function (map) {
-    //"this" returns to info.
-    this._div = L.DomUtil.create('div', 'info');
-    //the following line calls info.update(props) function. Again, this refers to 'info' here
-    this.update();
-    return this._div;
-};
-
-//Update the info based on what state user has clicked on
-info.update = function (props) {
-    this._div.innerHTML = '<h4>Built up area</h4>' + (props ?
-        '<b>' + props.NAME10 + '</b><br />' + Math.round(slideArr[index]) + '% BUA in ' + year[index]
-        : 'Hover over area');
-};
-
-function getColor(d) {
-    return d > 75 ? '#8c2d04' :
-           d > 62.5  ? '#cc4c02' :
-           d > 50  ? '#ec7014' :
-           d > 37.5  ? '#fe9929' :
-           d > 25   ? '#fec44f' :
-           d > 12.5  ? '#fee391' :
-           d > 6   ? '#fff7bc' :
-                      '#ffffe5';
-}
-function style(feature) {
-  var chlorArray = [
-    feature.properties.F1900_mean * 100,
-    feature.properties.F1910_mean * 100,
-    feature.properties.F1920_mean * 100,
-    feature.properties.F1930_mean * 100,
-    feature.properties.F1940_mean * 100,
-    feature.properties.F1950_mean * 100,
-    feature.properties.F1960_mean * 100,
-    feature.properties.F1970_mean * 100,
-    feature.properties.F1980_mean * 100,
-    feature.properties.F1990_mean * 100,
-    feature.properties.F2000_mean * 100,
-    feature.properties.F2010_mean * 100
-  ];
-    return {
-        fillColor: getColor(chlorArray[index]),
-        weight: 0.5,
-        opacity: 0.8,
-        color: 'black',
-        dashArray: '3',
-        fillOpacity: 0.7
-    };
-}
 //------------------------------------------------------------------------------------------------------------------
-
-function sliderArray(props){
-  slideArr = [
-    props.F1900_mean * 100,
-    props.F1910_mean * 100,
-    props.F1920_mean * 100,
-    props.F1930_mean * 100,
-    props.F1940_mean * 100,
-    props.F1950_mean * 100,
-    props.F1960_mean * 100,
-    props.F1970_mean * 100,
-    props.F1980_mean * 100,
-    props.F1990_mean * 100,
-    props.F2000_mean * 100,
-    props.F2010_mean * 100
-
-  ];
-};
-
 function createSlider(response){
     var Slider = L.Control.extend({
       options: {
@@ -127,7 +55,7 @@ function createSlider(response){
 map.addControl(new Slider());
 
 $('.range-slider').attr({
-  max: 11,
+  max: 12,
   min: 0,
   value: 0,
   step: 1
@@ -141,9 +69,45 @@ $('.range-slider').on('input', function(){
   });
 
 };
+//------------------------------------------------------------------------------------------------------------------
+var legend = L.control({position: 'bottomright'});
 
+legend.onAdd = function (map) {
 
-//-----------------------------------------------------------------------------------------------------------------
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 3.125, 6.25, 12.5, 25, 37.5, 50,62.5],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map);
+//------------------------------------------------------------------------------------------------------------------
+}; //end of create map function
+//------------------------------------------------------------------------------------------------------------------
+//When adding the info
+info.onAdd = function (map) {
+    //"this" returns to info.
+    this._div = L.DomUtil.create('div', 'info');
+    //the following line calls info.update(props) function. Again, this refers to 'info' here
+    this.update();
+    return this._div;
+};
+
+//Update the info based on what state user has clicked on
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Built up area</h4>' + (props ?
+        '<b>' + props.NAME10 + '</b><br />' + Math.round(slideArr[index]) + '% BUA in ' + year[index]
+        : 'Hover over area');
+};
+//------------------------------------------------------------------------------------------------------------------
 function hoverOn(e) {
     var layer = e.target;
 
@@ -177,3 +141,58 @@ function onEachFeature(feature, layer) {
       click: onClick
     });
 }
+//------------------------------------------------------------------------------------------------------------------
+function getColor(d) {
+    return d > 62.5 ? '#8c2d04' :
+           d > 50  ? '#cc4c02' :
+           d > 37.5  ? '#ec7014' :
+           d > 25  ? '#fe9929' :
+           d > 12.5   ? '#fec44f' :
+           d > 6.25  ? '#fee391' :
+           d > 3.125   ? '#fff7bc' :
+                      '#ffffe5';
+}
+//------------------------------------------------------------------------------------------------------------------
+function style(feature) {
+  var chlorArray = [
+    feature.properties.F1900_mean * 100,
+    feature.properties.F1910_mean * 100,
+    feature.properties.F1920_mean * 100,
+    feature.properties.F1930_mean * 100,
+    feature.properties.F1940_mean * 100,
+    feature.properties.F1950_mean * 100,
+    feature.properties.F1960_mean * 100,
+    feature.properties.F1970_mean * 100,
+    feature.properties.F1980_mean * 100,
+    feature.properties.F1990_mean * 100,
+    feature.properties.F2000_mean * 100,
+    feature.properties.F2010_mean * 100,
+    feature.properties.F2020_mean * 100
+  ];
+    return {
+        fillColor: getColor(chlorArray[index]),
+        weight: 0.5,
+        opacity: 0.8,
+        color: 'black',
+        fillOpacity: 0.7
+    };
+}
+//------------------------------------------------------------------------------------------------------------------
+function sliderArray(props){
+  slideArr = [
+    props.F1900_mean * 100,
+    props.F1910_mean * 100,
+    props.F1920_mean * 100,
+    props.F1930_mean * 100,
+    props.F1940_mean * 100,
+    props.F1950_mean * 100,
+    props.F1960_mean * 100,
+    props.F1970_mean * 100,
+    props.F1980_mean * 100,
+    props.F1990_mean * 100,
+    props.F2000_mean * 100,
+    props.F2010_mean * 100,
+    props.F2020_mean * 100
+
+  ];
+};
